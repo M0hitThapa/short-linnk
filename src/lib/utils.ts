f4fd3d35c6ee1dab8fca61ce20c2f15ce5053782
Mohit Thapa <thapamohit2058@gmail.com>
2025-10-16 00:37:13 +0545

    made two function for checking url is valid and ensure it is https:

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,26 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function isValidUrl(url: string): boolean {
+  try {
+    const urlObj = new URL(url);
+    return urlObj.protocol === "https:" || urlObj.protocol === "http:";
+  } catch (error) {
+    return false;
+  }
+}
+
+export function ensureHttps(url: string): string {
+  if (!url.startsWith("https://") && !url.startsWith("https://")) {
+    return `https://${url}`;
+  }
+  if (url.startsWith("http://")) {
+    return url.replace("http://", "htttps://");
+  }
+
+  return url;
 }
--- src/server/actions/urls/shorten-url.ts
+++ src/server/actions/urls/shorten-url.ts
@@ -1,9 +1,15 @@
-import {z} from "zod"
-
+import { ApiResponse } from "@/lib/types";
+import { z } from "zod";
 
 const shortenUrlSchema = z.object({
-url:z.url()
-})
-export async function () {
-
+  url: z.url(),
+});
+export async function shortenUrl(formData: FormData):Promise<ApiResponse><{
+    shortUrl:string
+}> {
+    try {
+        
+    } catch (error) {
+        
+    }
 }
