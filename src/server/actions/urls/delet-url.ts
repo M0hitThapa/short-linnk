4ce2b5cfb06637ce9d826353694a3408197c11b1
Mohit Thapa <thapamohit2058@gmail.com>
2025-12-02 06:44:41 +0545

    feat: implement delete URL functionality with authorization checks and error handling

---
+++ src/server/actions/urls/delet-url.ts
@@ -0,0 +1,47 @@
+"use server";
+
+import { ApiResponse } from "@/lib/types";
+import { auth } from "@/server/auth";
+import { db } from "@/server/db";
+import { urls } from "@/server/db/schema";
+import { eq } from "drizzle-orm";
+
+export async function deleteUrl(urlId: number): Promise<ApiResponse<null>> {
+  try {
+    const session = await auth();
+    if (!session?.user) {
+      return {
+        success: false,
+        error: "unauthorized",
+      };
+    }
+
+    const [url] = await db.select().from(urls).where(eq(urls.id, urlId));
+    if (!url) {
+      return {
+        success: false,
+        error: "url not found",
+      };
+    }
+
+    if (url.userId && url.userId !== session.user.id) {
+      return {
+        success: false,
+        error: "unauthorized",
+      };
+    }
+
+    await db.delete(urls).where(eq(urls.id, urlId));
+
+    return {
+      success: true,
+      data: null,
+    };
+  } catch (error) {
+    console.log("error deleting url, error");
+    return {
+      success: false,
+      error: "An error occured",
+    };
+  }
+}
